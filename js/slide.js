893a7e2499022a51b43916ab4d5783abb4b9d651
BAKHONGMIN <ghdals2655@naver.com>
2022-01-12 17:47:12 +0900

    3개 슬라이드

--- js/slide.js
+++ js/slide.js
@@ -1,28 +1,36 @@
+// /////////////////////////////////위사이드
 $(document).ready(function(){
+    var objTop = $("#topBanner ul");
 
     setInterval(function(){
-        $("#mainBanner ul").animate({
-        "marginTop" : "-="+350},400,
-        function(){
-            $("#mainBanner li").eq(0).appendTo($("#mainBanner ul"));
-            $("#mainBanner ul").css({"marginTop" : 0});
-        });
+        objTop.animate({
+            "marginTop":"-="+ 350},400,function(){
+                objTop.find('li').eq(0).appendTo(objTop);
+                objTop.css({"marginTop":0});
+            });
     },3000);
-    });
+});
+// //////////////////////////////////////////사이드
 
+$(document).ready(function(){
+    var objleft = $("#slideBanner ul");
 
+    setInterval(function(){
+        objleft.animate({
+            "marginLeft":"-="+ 1200},400,function(){
+                objleft.find('li').eq(0).appendTo(objleft);
+                objleft.css({"marginLeft":0});
+            });
+    },3000);
+});
+///////////////////////페이드
+$(document).ready(function(){
+    var objfade = $("#fadeBanner ul");
+    var count = 0;
 
-
-
-
-
-
-
-
-
-// {"marginLeft" : -1200},
-//     400,
-//     function(){
-//             // 애니메이션 끝나고 처리될 액션
-
-//     }
+    setInterval(function(){
+        count++;
+        count = count % 3;
+        objfade.find('li').eq(count).addClass('on').siblings().removeClass('on');
+    },3000);
+});
